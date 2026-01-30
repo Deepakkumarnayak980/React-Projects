48515f7be8fc9d73a40419637efffee1354d86db
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-01-30 22:40:51 +0530

    Movie List Structure  code

--- Cinement/src/pages/MovieList.jsx
+++ Cinement/src/pages/MovieList.jsx
@@ -1,5 +1,14 @@
+
+import { Card } from "../components/Card";
+
 export const MovieList = () => {
   return (
-    <main>MovieList</main>
-  )
-}
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+         <Card />
+        </div>
+      </section>
+    </main>
+  );
+};
